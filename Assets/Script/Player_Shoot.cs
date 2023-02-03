19e3ead2d24852fb053638844b07faa84b4db541
Indunil Suresh <102247770+Isuresh98@users.noreply.github.com>
2023-02-04 00:50:21 +0530

    34 complete

---
+++ Assets/Script/Player_Shoot.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_Shoot : MonoBehaviour
+{
+    public GameObject _bulatPrefabs;
+    public GameObject bularPoint;
+    public float _bulatSpeed;
+    Vector3 mousePos;
+    Vector3 worlPos;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Shoot();
+    }
+    private void Shoot()
+    {
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            mousePos = Input.mousePosition;
+            worlPos = Camera.main.ScreenToWorldPoint(mousePos);
+            Vector2 roundPos = new Vector2(Mathf.RoundToInt(worlPos.x), Mathf.RoundToInt(worlPos.y));
+
+            GameObject bullat = Instantiate(_bulatPrefabs, bularPoint.transform.position, Quaternion.identity);
+
+            Rigidbody2D _rbody = bullat.GetComponent<Rigidbody2D>();
+            Vector2 direction = (roundPos - (Vector2) bularPoint.transform.position).normalized;
+            _rbody.velocity = direction * _bulatSpeed * Time.deltaTime;
+            Destroy(bullat, 2f);
+            
+
+        }
+    }
+}
