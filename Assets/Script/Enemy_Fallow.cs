e507c521b9a3fe505245a604ed55cbdcd126e356
Indunil Suresh <102247770+Isuresh98@users.noreply.github.com>
2023-02-09 23:38:15 +0530

    40 update enemy fowerd only set

--- Assets/Script/Enemy_Fallow.cs
+++ Assets/Script/Enemy_Fallow.cs
@@ -4,16 +4,18 @@ using UnityEngine;
 
 public class Enemy_Fallow : MonoBehaviour
 {
-    public float Speed = 3f;
+    public float Speed = 25f;
     public float StopDistans = 3f;
 
     private GameObject _targetPlayer;
     private float _distans;
+    private Rigidbody2D _rBody;
     // Start is called before the first frame update
     void Start()
     {
         _targetPlayer = GameObject.FindWithTag("Player");
-     
+
+        _rBody = GetComponent<Rigidbody2D>();
     }
     // Update is called once per frame
     void Update()
@@ -23,13 +25,17 @@ public class Enemy_Fallow : MonoBehaviour
 
     private void FlowwoEnemy()
     {
-        if (_targetPlayer != null)
-        {
-            _distans = Vector2.Distance(transform.position, _targetPlayer.transform.position);
-            if (_distans > StopDistans)
-            {
-                transform.position = Vector2.MoveTowards(transform.position, _targetPlayer.transform.position, Speed * Time.deltaTime);
-            }
-        }
+        _rBody.AddForce(transform.up* Speed* Time.deltaTime);
+        Destroy(this.gameObject, 5f);
+
+        /* if (_targetPlayer != null)
+         {
+             _distans = Vector2.Distance(transform.position, _targetPlayer.transform.position);
+             if (_distans > StopDistans)
+             {
+                 transform.position = Vector2.MoveTowards(transform.position, _targetPlayer.transform.position, Speed * Time.deltaTime);
+             }
+         }
+        */
     }
 }
